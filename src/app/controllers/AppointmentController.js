df2bef7b1e41f108c77945042c51b69d8fb3cbfb
RenanArantes <renan.arantes@rede.ulbra.br>
2020-02-05 07:28:37 -0300

    Adicao Paginacao  de Agendamentos e Listagem de  Prestadores

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -2,8 +2,37 @@ import * as Yup from 'yup';
 import { startOfHour, parseISO, isBefore } from 'date-fns';
 import Appointment from '../models/Appointment';
 import User from '../models/User';
+import File from '../models/File';
 
 class AppointmentController {
+    async index(req, res) {
+        const { page = 1 } = req.query;
+
+        const appointments = await Appointment.findAll({
+            where: { user_id: req.userId, canceled_at: null },
+            order: ['date'],
+            attributes: ['id', 'date'],
+            limit: 20,
+            offset: (page - 1) * 20,
+            include: [
+                {
+                    model: User,
+                    as: 'provider',
+                    attributes: ['id', 'name'],
+                    include: [
+                        {
+                            model: File,
+                            as: 'avatar',
+                            attributes: ['id', 'path', 'url'],
+                        },
+                    ],
+                },
+            ],
+        });
+
+        return res.json(appointments);
+    }
+
     async store(req, res) {
         const schema = Yup.object().shape({
             provider_id: Yup.number().required(),
@@ -39,7 +68,7 @@ class AppointmentController {
                 .status(400)
                 .json({ error: 'Past dates are not permitted' });
         }
-        console.log(`Hora formatada: ${hourStart} | Hora passada: ${date}`);
+
         /**
          * Check date availability
          */
@@ -50,7 +79,7 @@ class AppointmentController {
                 date: hourStart,
             },
         });
-        console.log(checkAvailability);
+
         if (checkAvailability) {
             return res
                 .status(400)
