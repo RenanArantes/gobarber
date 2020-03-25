7dcb94087c22ba246afc923941a12aa3d1971de5
RenanArantes <renan.arantes@rede.ulbra.br>
2020-03-24 21:10:51 -0300

    notificatin e prfile editin style

--- web/src/pages/Profile/styles.js
+++ web/src/pages/Profile/styles.js
@@ -1,18 +1,9 @@
 import styled from 'styled-components';
 import { darken } from 'polished';
 
-export const Wrapper = styled.div`
-    height: 100%;
-    background: linear-gradient(-90deg, #7159c1, #1fc577);
-    display: flex;
-    justify-content: center;
-    align-items: center;
-`;
-
-export const Content = styled.div`
-    width: 100%;
-    max-width: 315px;
-    text-align: center;
+export const Container = styled.div`
+    max-width: 600px;
+    margin: 50px auto;
 
     form {
         display: flex;
@@ -29,7 +20,10 @@ export const Content = styled.div`
             margin: 0 0 10px;
 
             &::placeholder {
-                color: rgba(255, 255, 255, 0.7);
+                color: rgba(255, 255, 255, 0.5);
+            }
+            &:hover {
+                background: ${darken(0.1, 'rgba(255, 255, 255, 0.3)')};
             }
         }
 
@@ -56,6 +50,13 @@ export const Content = styled.div`
             font-weight: bold;
         }
 
+        hr {
+            border: 0;
+            height: 1px;
+            background: rgba(255, 255, 255, 0.2);
+            margin: 10px 0 20px;
+        }
+
         a {
             color: #fff;
             margin-top: 15px;
@@ -65,6 +66,23 @@ export const Content = styled.div`
             &:hover {
                 opacity: 1;
             }
+        }
+    }
+
+    > button {
+        width: 100%;
+        margin: 10px 0 0;
+        height: 44px;
+        background: #f64c75;
+        font-weight: bold;
+        color: #fff;
+        border: 0;
+        border-radius: 4px;
+        font-size: 16px;
+        transition: background 0.2s;
+
+        &:hover {
+            background: ${darken(0.1, '#f64c75')};
         }
     }
 `;
--- web/src/pages/_laytouts/default/styles.js
+++ web/src/pages/_laytouts/default/styles.js
@@ -2,5 +2,5 @@ import styled from 'styled-components';
 
 export const Wrapper = styled.div`
     height: 100%;
-    background: linear-gradient(-90deg, #7159c1, #0f0);
+    background: linear-gradient(-90deg, #7159c1, #1fc577);
 `;
